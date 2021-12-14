4177f27673448af8b7f17419dd043f7dd2efe648
Ami Asokumar <gasokumar@gmail.com>
2021-12-13 23:56:35 -0500

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -15,9 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://localhost/budget-tracker", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 // routes
