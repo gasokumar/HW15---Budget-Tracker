725abcb368f50161451dd4f7685be5addb1cc33b
Ami Asokumar <gasokumar@gmail.com>
2021-12-19 23:15:12 -0500

    finished db.js

--- public/db.js
+++ public/db.js
@@ -1,3 +1,4 @@
+// https://www.youtube.com/watch?v=VNFDoawcmNc Video on using IndexedDB that helped me
 // Creating an empty db variable
 let db;
 
@@ -7,5 +8,68 @@ const request = indexedDB.open("budget", 1);
 // Creating an object store named pending in indexedDB.
 request.onupgradeneeded = function (event) {
   const db = event.target.result;
-  db.createObjectStore("pending transactions", { autoIncrement: true });
+  db.createObjectStore("pendingTransactions", { autoIncrement: true });
 };
+
+// When the database is opened successfully, check if our app is online. If it's online, check the database to see if there's anything in it and post the results to our online database if there is.
+request.onsuccess = function (event) {
+  db = event.target.result;
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+// saveRecord function found in index.js line 139. This function saves data to our object store
+function saveRecord(transaction) {
+  // Opening a transaction in the object store pendingTransactions
+  const transaction = db.transaction(["pendingTransactions"], "readwrite");
+  //   Linking the transaction to the object store
+  const store = transaction.objectStore("pendingTransactions");
+  store.add(transaction);
+}
+
+// This function checks to see if there's anything in our indexedDB (used to store transactions when our app is offline). If there's anything in our database, it posts the data to our database.
+function checkDatabase() {
+  // Opening a transaction in the object store pendingTransactions
+  const transaction = db.transaction(["pendingTransactions"], "readwrite");
+  //   Opening the object store for the transaction
+  const store = transaction.objectStore("pendingTransactions");
+  //   Getting all the data in the object store
+  const getAll = store.getAll();
+
+  //   If there's anything in the object store, post the results to our database using the /api/transaction/bulk path.
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          // delete records if successful
+          const transaction = db.transaction(
+            ["pendingTransactions"],
+            "readwrite"
+          );
+          const store = transaction.objectStore("pendingTransactions");
+          store.clear();
+        });
+    }
+  };
+}
+// This function deletes the data in our object store.
+function deleteStore() {
+  // Opening a transaction
+  const transaction = db.transaction(["pendingTransactions"], "readwrite");
+  //   Opening the object store for the transaction
+  const store = transaction.objectStore("pendingTransactions");
+  //   clearing the object store
+  store.clear();
+}
+
+// When the app comes back online, run checkDatabase (which posts transactions that have been stored offline to our online database)
+window.addEventListener("online", checkDatabase);
