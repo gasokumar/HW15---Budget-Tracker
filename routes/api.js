6966fb1e90073e35cf9e1885771609c2603078f2
Ami Asokumar <gasokumar@gmail.com>
2021-12-28 11:39:35 -0500

    added comments/trying to get heroku deployment working

--- routes/api.js
+++ routes/api.js
@@ -22,7 +22,7 @@ router.post("/api/transaction/bulk", ({ body }, res) => {
     });
 });
 
-// Returning to the user the transactions.
+// Returning transactions to the user
 router.get("/api/transaction", (req, res) => {
   Transaction.find({})
     .sort({ date: -1 })
